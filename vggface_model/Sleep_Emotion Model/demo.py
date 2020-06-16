1225a487c149a17b479667162b99f60e354d8f92
Ahmad Ganzouri <1500121@eng.asu.edu.eg>
2020-06-17 01:14:28 +0200

    Update the interface
    Updating the demo to have more obvious and modern
    interface for the driver's emotion and sleep detection.
    Alerting the driver when he become drowsy for a while

--- vggface_model/Sleep_Emotion Model/demo.py
+++ vggface_model/Sleep_Emotion Model/demo.py
@@ -1,15 +1,17 @@
-from keras.models import load_model
 import cv2
 import numpy as np
+from keras.models import load_model
 
-emotion_dict = {0: "Angry", 1: "Happy", 2: "neutral", 3: "sad"}
+emotion_dict = {0: "angry", 1: "happy", 2: "neutral", 3: "sad"}
 sleep_dict = {0: "alert", 1: "drowsy"}
 
 model = load_model("sleep_emotion_model.hdf5")
 
 cap = cv2.VideoCapture(0)
 i = 0
-cv2.namedWindow("window", cv2.WINDOW_NORMAL)
+cv2.namedWindow("Driver assistant", cv2.WINDOW_NORMAL)
+count = 1
+sleep_prev = str("awake")
 
 while True:
 
@@ -25,11 +27,34 @@ while True:
         cropped_img = np.expand_dims(cv2.resize(roi, (224, 224)), 0)
 
         prediction = model.predict(cropped_img)
-        sleep = prediction[0]
-        emotion = prediction[1]
-        cv2.putText(frame, emotion_dict[int(np.argmax(emotion))] + ", " + sleep_dict[int(np.argmax(sleep))], (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
+        sleep = sleep_dict[int(np.argmax(prediction[0]))]
+        emotion = emotion_dict[int(np.argmax(prediction[1]))]
 
-    cv2.imshow('window', frame)
+        if sleep == "drowsy" and sleep == sleep_prev:
+            count = count+1
+            sleep_prev = sleep
+            if count > 15:
+                cv2.rectangle(frame, (x + w + 20, y + 40), (x + w + 120, y + 55), (28, 13, 191), 13)
+                cv2.putText(frame, "ALERT!!", (x + w + 32, y + 54), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 1,
+                            cv2.LINE_AA)
+        else:
+            sleep_prev = sleep
+            count = 1
+
+        cv2.rectangle(frame, (x + w + 20, y + 5), (x + w + 120, y + 20), (206, 174, 17), 13)
+        cv2.putText(frame, emotion, (x + w + 32, y + 18), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 1,
+                    cv2.LINE_AA)
+
+        if sleep == "awake":
+            cv2.rectangle(frame, (x + w + 20, y + 40), (x + w + 120, y + 55), (206, 174, 17), 13)
+            cv2.putText(frame, sleep, (x + w + 32, y + 53), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 1,
+                        cv2.LINE_AA)
+        elif sleep == "drowsy" and count <= 10:
+            cv2.rectangle(frame, (x + w + 20, y + 40), (x + w + 120, y + 55), (14, 88, 235), 13)
+            cv2.putText(frame, sleep, (x + w + 32, y + 53), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 1,
+                        cv2.LINE_AA)
+
+    cv2.imshow('Driver assistant', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
